5f110745ae3a0e8a7aa01d48dd3a72ec888d1596
sreekanth gunda <sreekanthgunda18@gmail.com>
2026-05-15 11:22:09 +0530

    Improved error response

--- src/main/java/com/employee/exception/GlobalExceptionHandler.java
+++ src/main/java/com/employee/exception/GlobalExceptionHandler.java
@@ -4,29 +4,51 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.employee.dto.ErrorResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<?> handleResourceNotFound(ResourceNotFoundException ex) {
-		Map<String, Object> error = new HashMap<>();
-		error.put("timestamp", LocalDateTime.now());
-		error.put("message", ex.getMessage());
-		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-	}
+	public ResponseEntity<ErrorResponse> handleResourceNotFound(
+			ResourceNotFoundException ex,
+			HttpServletRequest request) {
 
+		ErrorResponse response = new ErrorResponse(
+				LocalDateTime.now(),
+				HttpStatus.NOT_FOUND.value(),
+				"RESOURCE_NOT_FOUND",
+				ex.getMessage(),
+				null, // fieldErrors not needed here
+				request.getRequestURI()
+		);
+
+		return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+	}
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<?> handleValidation(MethodArgumentNotValidException ex) {
+	public ResponseEntity<ErrorResponse> handleValidation(MethodArgumentNotValidException ex, HttpServletRequest request) {
 		Map<String, String> errors = new HashMap<>();
+
 		ex.getBindingResult()
-		.getFieldErrors()
-		.forEach(error -> errors.put(error.getField(),error.getDefaultMessage()));
-		return ResponseEntity.badRequest().body(errors);
+				.getFieldErrors()
+				.forEach(error ->
+						errors.put(error.getField(), error.getDefaultMessage()));
+
+		ErrorResponse response = new ErrorResponse(
+				LocalDateTime.now(),
+				HttpStatus.BAD_REQUEST.value(),
+				"Validation Failed",
+				"Input validation failed for request",
+				errors,
+				request.getRequestURI()
+		);
+
+		return ResponseEntity.badRequest().body(response);
 	}
 }
